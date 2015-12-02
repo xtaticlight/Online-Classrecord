84ade5eff763faff1fbd9c17cc12c888c6d3a2f6
Sui~yans <Sui~yans@Suiyans-PC>
2015-12-02 23:36:14 +0800

    Partial Commit

--- app/Http/Controllers/SigningController.php
+++ app/Http/Controllers/SigningController.php
@@ -7,41 +7,39 @@ use Illuminate\Support\Facades;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\User;
+use DB;
 
 class SigningController extends Controller {
+
     protected $loginPath = '/login';
-    
-    public function __construct()
-	{
-		$this->middleware('guest');
-	}
-    
+
+    public function __construct() {
+        $this->middleware('guest');
+    }
+
     function getData($username) {
-        $userData = User::where('username', '=', $username)->first();
-        $employee_id = $userData['employee_id'];
-        
-        $user_data = array(
-            'employee_id' => $userData['employee_id'],
-            'name' => $userData['name'],
-            'gender' => $userData['gender'],
-            'email' => $userData['email'],
-            'contact' => $userData['contact'],
-            'username' => $userData['username'],
-            'password' => $userData['password'],
-            'usertype' => $userData['usertype'],
-            'department' => $userData['department'],
-            'college' => $userData['college'],
-        );
-        return $user_data;
+
+        $query = DB::table('users')->where('username', '=', $username)->first();
+        $resultArray = json_decode(json_encode($query), true);
+        // dd($resultArray);
+        //$userData = User::where('username', '=', $username)->first();
+        return $resultArray;
+    }
+
+    function getCourse($users_id) {
+        $query = DB::table('courses')->where('users_id', '=', $users_id)->get();
+        $resultArray = json_decode(json_encode($query), true);
+      //  dd($resultArray);
+        return $resultArray;
     }
 
     function postSignin() {
         $username = \Input::get('username');
         $password = \Input::get('password');
-        
+
         $userData = $this->getData($username);
-        
-        
+
+
         if (is_null($username) || (empty($username))) {
             return \Redirect::back()
                             ->with('message', 'Null talisman.<br> ')
@@ -62,30 +60,37 @@ class SigningController extends Controller {
                             ->withInput();
         }
     }
+
     function showAccess() {
-      $username = \Input::get('username');
-      $userData = $this->getData($username);
-      return view('pages.access')->with('data', $userData);
+        $id = \Input::get('id');
+        $username = \Input::get('username');
+        $userData = $this->getData($username);
+        $courseData = $this->getCourse($id);
+        return view('pages.access')->with('userData', $userData)->with('courseData', $courseData);
     }
+
     function showSubjects() {
-      $username = \Input::get('username');
-      $userData = $this->getData($username);
-      return view('pages.subjects')->with('data', $userData);
+        $username = \Input::get('username');
+        $id = \Input::get('id');
+        $userData = $this->getData($username);
+        $subjectData = $this->getCourse($id);
+        return view('pages.subjects')->with('userData', $userData)->with('subjectData', $subjectData);
     }
+
     function showRecords() {
-       
-      $username = \Input::get('username');
-      $userData = $this->getData($username);
-      $quiz11 = null;
-      $tquiz11 = null;
-      $quiz12 = null;
-      $tquiz12 = null;
-      $prelimexam = null;
-      $tprelimexam = null;
-      $midtermexam = null;
-      $tmidtermexam = null;
-      $midtermgrade = null;
-      $records = array(
+
+        $username = \Input::get('username');
+        $userData = $this->getData($username);
+        $quiz11 = null;
+        $tquiz11 = null;
+        $quiz12 = null;
+        $tquiz12 = null;
+        $prelimexam = null;
+        $tprelimexam = null;
+        $midtermexam = null;
+        $tmidtermexam = null;
+        $midtermgrade = null;
+        $records = array(
             'quiz11' => $quiz11,
             'tquiz11' => $tquiz11,
             'quiz12' => $quiz12,
@@ -96,22 +101,23 @@ class SigningController extends Controller {
             'tmidtermexam' => $tmidtermexam,
             'midtermgrade' => $midtermgrade,
         );
-      return view('pages.records')->with('records', $records)->with('data', $userData);
+        return view('pages.records')->with('records', $records)->with('data', $userData);
     }
+
     function getSolve() {
         $username = \Input::get('username');
-      $userData = $this->getData($username);
-      
-      $quiz11 = \Input::get('Quiz11');
-      $tquiz11 = \Input::get('TQuiz11');
-      $quiz12 = \Input::get('Quiz12');
-      $tquiz12 = \Input::get('TQuiz12');
-      $prelimexam = \Input::get('PrelimExam');
-      $tprelimexam = \Input::get('TPrelimExam');
-      $midtermexam = \Input::get('MidtermExam');
-      $tmidtermexam = \Input::get('TMidtermExam');
-      $midtermgrade = (((5 - (4 * ($quiz11)) / $tquiz11) + (5 - (4 * ($quiz12)) / $tquiz12) + (5 - (4 * ($prelimexam)) / $tprelimexam)) / 3 * 0.4) + ((5 - (4 * ($midtermexam)) / $tmidtermexam) * 0.6);
-      $records = array(
+        $userData = $this->getData($username);
+
+        $quiz11 = \Input::get('Quiz11');
+        $tquiz11 = \Input::get('TQuiz11');
+        $quiz12 = \Input::get('Quiz12');
+        $tquiz12 = \Input::get('TQuiz12');
+        $prelimexam = \Input::get('PrelimExam');
+        $tprelimexam = \Input::get('TPrelimExam');
+        $midtermexam = \Input::get('MidtermExam');
+        $tmidtermexam = \Input::get('TMidtermExam');
+        $midtermgrade = (((5 - (4 * ($quiz11)) / $tquiz11) + (5 - (4 * ($quiz12)) / $tquiz12) + (5 - (4 * ($prelimexam)) / $tprelimexam)) / 3 * 0.4) + ((5 - (4 * ($midtermexam)) / $tmidtermexam) * 0.6);
+        $records = array(
             'quiz11' => $quiz11,
             'tquiz11' => $tquiz11,
             'quiz12' => $quiz12,
@@ -120,8 +126,9 @@ class SigningController extends Controller {
             'tprelimexam' => $tprelimexam,
             'midtermexam' => $midtermexam,
             'tmidtermexam' => $tmidtermexam,
-            'midtermgrade' => round($midtermgrade,2),
+            'midtermgrade' => round($midtermgrade, 2),
         );
-      return view('pages.records')->with('records', $records)->with('data', $userData);
+        return view('pages.records')->with('records', $records)->with('data', $userData);
     }
+
 }
